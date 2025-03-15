65c93aafef275dd1490d52e5756d3f1ebd5c5432
loveday.melo <loveday.melo@localhost>
2025-03-15 10:28:03 +0100

    Third commit

--- LearnJava/src/exceptionhandling/ExceptionHandling.java
+++ LearnJava/src/exceptionhandling/ExceptionHandling.java
@@ -3,22 +3,23 @@ package exceptionhandling;
 public class ExceptionHandling {
 	public static void main(String[] args) {
 		try {
+			demo();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		}
+	public static void demo() throws Exception {
+		
 			System.out.println("I am Great Edhughoro....");
-			int i = 1/0;
-			System.out.println("Completed");
-		}
-		catch(Exception exp) {
-			System.out.println("I am inside catch block");
-			System.out.println("Message is : " +exp.getMessage());
-			System.out.println("Cause is : " +exp.getCause());
-exp.printStackTrace();
-
-			
-		     }
-		finally {
-			System.out.println("I am inside finally block");
-		}
-		}
+			throw new ArithmeticException("not valid operation");
+			//int i = 1/0;
+			//System.out.println("Completed");
+		
+		
+		
+	}
 		
 	}
 
